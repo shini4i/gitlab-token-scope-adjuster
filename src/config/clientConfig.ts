3d0cbd1ea46c33935f01399ac9053044fb42b1f9
Vadim Gedz <vadims@linux-tech.io>
2024-05-29 01:19:07 +0300

    chore(config): remove unused ProjectID (#2)

--- src/config/clientConfig.ts
+++ src/config/clientConfig.ts
@@ -16,7 +16,6 @@ export function NewClientConfig(): Config {
     const plainConfig = {
         Url: process.env.GITLAB_URL,
         Token: process.env.GITLAB_TOKEN,
-        ProjectID: process.env.GITLAB_PROJECT_ID ? parseInt(process.env.GITLAB_PROJECT_ID) : undefined,
     };
 
     const config = plainToInstance(Config, plainConfig);
